0ce9daa5af27fa3aab2857ed70c93fb5a25b623f
kevin.qu <kevin.qu326@gmail.com>
2014-05-21 16:35:33 +0800

    delete

--- asm_plugin_src_1.0.1_for_java_1_4/ASMPlugin/jni/create_header.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-del winapi.h > nul
-cd ..\bin
-cls
-javah -jni -classpath . -o winapi.h de.fh_zwickau.asmplugin.WinApi
-echo.
-copy winapi.h ..\jni\winapi.h > nul
-del winapi.h > nul
-pause
